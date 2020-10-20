28def52b7fe8f68035b6cd86e3da9ea3a279fe34
Ahmad Adelaja <ahmadadelaja@gmail.com>
2020-10-20 07:42:23 +0100

    Add a new student activity initial display completion

---
+++ app/src/main/java/com/github/ugwulo/afrilang/AddStudentActivity.java
@@ -0,0 +1,22 @@
+package com.github.ugwulo.afrilang;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class AddStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_student);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
