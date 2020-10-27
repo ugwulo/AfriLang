22257832fb9864c63e7c51d6f0aca7e69b5ee14d
ugwulo <seanlite94@gmail.com>
2020-10-28 00:40:51 +0100

    fix issue #4 and #6 add:    app/src/main/java/com/github/ugwulo/afrilang/PrivateUserAssessmentActivity.java add:   app/src/main/java/com/github/ugwulo/afrilang/PrivateUserProfileActivity.java

---
+++ app/src/main/java/com/github/ugwulo/afrilang/PrivateUserAssessmentActivity.java
@@ -0,0 +1,30 @@
+package com.github.ugwulo.afrilang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.github.ugwulo.afrilang.databinding.ActivityPrivateUserAssessmentBinding;
+
+/** @author Ndubuisi Ugwulo **/
+/** {@link PrivateUserAssessmentActivity} enables private users to take tests **/
+public class PrivateUserAssessmentActivity extends AppCompatActivity {
+
+    ActivityPrivateUserAssessmentBinding mBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBinding = ActivityPrivateUserAssessmentBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        populateTopicSelectionSpinner();
+    }
+
+    private void populateTopicSelectionSpinner() {
+        ArrayAdapter<CharSequence> topicAdapter = ArrayAdapter.createFromResource(this, R.array.topic, R.layout.spinner_text);
+        topicAdapter.setDropDownViewResource(R.layout.spinner_drop_down);
+    }
+}
---
+++ app/src/main/java/com/github/ugwulo/afrilang/PrivateUserProfileActivity.java
@@ -0,0 +1,16 @@
+package com.github.ugwulo.afrilang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/** @author Ndubuisi Ugwulo **/
+/** {@link PrivateUserProfileActivity} displays a private user's profile **/
+public class PrivateUserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_private_user_profile);
+    }
+}
