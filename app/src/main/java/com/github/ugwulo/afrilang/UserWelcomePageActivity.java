125abda2573db79cc38c21877cc2a8ee2f835a28
miichaelOI <52916666+miichaelOI@users.noreply.github.com>
2020-10-29 17:35:28 +0100

    michael's  commit

---
+++ app/src/main/java/com/github/ugwulo/afrilang/UserWelcomePageActivity.java
@@ -0,0 +1,37 @@
+package com.github.ugwulo.afrilang;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class UserWelcomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_welcome_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
